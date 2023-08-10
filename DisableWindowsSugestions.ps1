8a8676d5eb8c1874e22f5630d4601209fa6d24ba
Ronald <vanheugten.ronald@gmail.com>
2023-08-10 14:19:27 +0200

    Create DisableWindowsSugestions.ps1

---
+++ DisableWindowsSugestions.ps1
@@ -0,0 +1,17 @@
+* https://gist.github.com/carcheky/981fda4af8e5aac73d0ca8558947ffbb
+- * https://answers.microsoft.com/en-us/windows/forum/all/turn-off-windows-spotlight-fun-facts/f96d5c6e-3150-469c-bc0f-d847ba0149b4
+- * https://answers.microsoft.com/en-us/windows/forum/all/turn-off-windows-spotlight-fun-facts/f96d5c6e-3150-469c-bc0f-d847ba0149b4
+- * https://www.tenforums.com/tutorials/30869-turn-off-tip-trick-suggestion-notifications-windows-10-a.html
+- * https://techjourney.net/disable-show-suggestions-occasionally-in-start-in-windows-10/
+- * https://superuser.com/questions/1406686/keep-windows-10-lock-screen-spotlight-pictures-but-turn-off-all-texts-hints-ball
+- * https://www.tenforums.com/tutorials/100541-turn-off-suggested-content-settings-app-windows-10-a.html
+- * https://www.tenforums.com/tutorials/24117-turn-off-app-suggestions-start-windows-10-a.html
+[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager]
+"RotatingLockScreenEnabled"=dword:00000000
+"RotatingLockScreenOverlayEnabled"=dword:00000000
+"SubscribedContent-338387Enabled"=dword:00000000
+"SubscribedContent-338388Enabled"=dword:00000000
+"SubscribedContent-338389Enabled"=dword:00000000
+"PreInstalledAppsEverEnabled"=dword:00000000
+"SystemPaneSuggestionsEnabled"=dword:00000000
+Bonus: "SlideshowEnabled"=dword:00000000
