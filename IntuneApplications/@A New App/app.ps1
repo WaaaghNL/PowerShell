b83374443061c0c3908ef41012f84629fade5051
Ronald <vanheugten.ronald@gmail.com>
2023-08-11 11:32:30 +0200

    Create app.ps1

---
+++ IntuneApplications/@A New App/app.ps1
@@ -0,0 +1,27 @@
+## How to use:
+## Place this script in the root of the aquired zip file from SecTec (there shoud be two folders named install and uninstall) and package the application as a intunewin file.
+## 
+## Install command:   powershell.exe -ExecutionPolicy Bypass -file app.ps1 -Mode Install
+## Uninstall command: powershell.exe -ExecutionPolicy Bypass -file app.ps1 -Mode Uninstall
+##
+## Create the folowing detection rule
+## Rules format:     Manually configure detection rules
+## Rule type:        File
+## Path:             C:\Program Files\Application
+## File or folder:   App.exe
+## Detection method: File or folder exists
+
+Param(
+	[Parameter(Mandatory=$true, HelpMessage="Accepted values: Install, and Uninstall")]
+	[ValidateSet("Install", "Uninstall")]
+	[String[]]
+	$Mode
+)
+ 
+if ($Mode -eq "Install"){
+    ## Do Install Things
+}
+ 
+if ($Mode -eq "Uninstall"){
+	## Do Uninstall Things
+}
