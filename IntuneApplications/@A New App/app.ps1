959b3797e6f1c1df3a86f725e88351014e32a4ff
Ronald <hallo@ronaldvanheugten.nl>
2024-03-08 13:30:57 +0100

    Update app.ps1

--- IntuneApplications/@A New App/app.ps1
+++ IntuneApplications/@A New App/app.ps1
@@ -8,12 +8,21 @@
 ## Install command:   powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -file app.ps1 -Mode Install
 ## Uninstall command: powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -file app.ps1 -Mode Uninstall
 ##
-## Create the folowing detection rule
+## Create the folowing detection rule (file)
 ## Rules format:     Manually configure detection rules
 ## Rule type:        File
 ## Path:             C:\Program Files\Application
 ## File or folder:   App.exe
 ## Detection method: File or folder exists
+##
+## Create the folowing detection rule (register)
+## Rules format:     Manually configure detection rules
+## Rule type:         Registry
+## Keypath:          keypath
+## Value name:       Version
+## Detection method: String comparison
+## Operator:         Equals
+## Value:            Version number like 6.0.27
 
 ##
 ## Parameters, Sorry you can't create code above this. Blame Microsoft!
