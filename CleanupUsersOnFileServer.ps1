18e41c7bef539b8e98228d3fa87237d6b0923623
Ronald <hallo@ronaldvanheugten.nl>
2024-08-27 18:14:36 +0200

    Add files via upload

---
+++ CleanupUsersOnFileServer.ps1
@@ -0,0 +1,50 @@
+# Definieer de drie mappen die je wilt controleren
+$directories = @("\\fileserver01\c$\Profiles", "c:\UserProfiles")
+
+# Naar welke submap verplaatsen we de oude profielen?
+$RemovedFolderName = "@Directories van verwijderde gebruikers"
+
+# Zoek alle sAMAccountName hebbende objecten in AD
+$searcher = New-Object System.DirectoryServices.DirectorySearcher
+$searcher.Filter = "(objectClass=user)"
+$searcher.PropertiesToLoad.Add("sAMAccountName") | Out-Null
+
+# Haal alle gebruikersnamen op van de beschikbare gebruikers.
+$adUsers = @()
+$results = $searcher.FindAll()
+foreach ($result in $results) {
+    $adUsers += $result.Properties["sAMAccountName"]
+}
+
+foreach ($directory in $directories) {
+    # Kijk of de map benaderbaar is
+    if (Test-Path -Path $directory) {
+        # Maak de map aan voor oude profielen als deze niet bestaat
+        $nonMatchingDir = Join-Path -Path $directory -ChildPath $RemovedFolderName
+        if (-not (Test-Path -Path $nonMatchingDir)) {
+            New-Item -Path $nonMatchingDir -ItemType Directory
+        }
+
+        # Maak een lijst van alle mapnamen in de array map
+        $items = Get-ChildItem -Path $directory -Directory
+
+        foreach ($item in $items) {
+            # Pak alleen de map naam
+            $itemName = $item.Name
+
+            # Sla de map voor oude gebruikers over
+            if ($itemName -eq $RemovedFolderName) {
+                continue
+            }
+
+            # Kijk of de map naam voorkomt in de ad gebruikers username array
+            if (-not ($adUsers -contains $itemName)) {
+                # Zo niet verplaats de map naar de oude gebruikers folder zodat deze ter beoordeling beschikbaar is
+                $destination = Join-Path -Path $nonMatchingDir -ChildPath $itemName
+                Move-Item -Path $item.FullName -Destination $destination -Force
+            }
+        }
+    } else {
+        Write-Host "Geen toegang tot map $directory ."
+    }
+}
